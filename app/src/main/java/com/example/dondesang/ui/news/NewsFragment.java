d5510f1c199b687a2baccb13ff89e76d8d38c451
tonyMICCELI <tony.cuillandre@gmail.com>
2022-12-05 02:03:26 +0100

    Create News feature

--- app/src/main/java/com/example/dondesang/ui/news/NewsFragment.java
+++ app/src/main/java/com/example/dondesang/ui/news/NewsFragment.java
@@ -4,13 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+//import androidx.lifecycle.ViewModelProvider;
 
+import com.example.dondesang.R;
 import com.example.dondesang.databinding.FragmentNewsBinding;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class NewsFragment extends Fragment {
 
@@ -18,14 +27,37 @@ public class NewsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NewsViewModel newsViewModel =
-                new ViewModelProvider(this).get(NewsViewModel.class);
+        //NewsViewModel newsViewModel =
+                //new ViewModelProvider(this).get(NewsViewModel.class);
 
         binding = FragmentNewsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNews;
-        newsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        //final TextView textView = binding.textNews;
+        //newsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        final EditText editText = binding.userNews;
+        final Button sendButton = binding.sendButton;
+        LinearLayout newsLayout = binding.newsZone;
+        ScrollView scrollView = binding.scrollView;
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!editTextIsEmpty(editText)){
+                    String newsContent = editText.getText().toString();
+                    View newPost = getLayoutInflater().inflate(R.layout.news_card, null);
+                    newsLayout.addView(newPost);
+                    TextView textView = (TextView) newPost.findViewById(R.id.messageView);
+                    textView.setText(newsContent);
+                    TextView dateView = (TextView) newPost.findViewById(R.id.dateView);
+                    dateView.setText(getCurrentDate());
+                    scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+                    editText.getText().clear();
+                    editText.onEditorAction(EditorInfo.IME_ACTION_DONE);
+                }
+            }
+        });
         return root;
     }
 
@@ -33,5 +65,14 @@ public class NewsFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private boolean editTextIsEmpty(EditText editText) {
+        return editText.getText().toString().trim().length() == 0;
+    }
+
+    private String getCurrentDate() {
+        Date currentTimeDate = Calendar.getInstance().getTime();
+        return currentTimeDate.toString();
     }
 }
