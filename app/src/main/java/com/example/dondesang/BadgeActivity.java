eb99122c37e48f400bc6fb6bbe5046b298ff75a3
ThomasClvr <99747179+ThomasClvr@users.noreply.github.com>
2022-12-07 18:09:01 -0500

    BadgeActivity
    Début Badges

---
+++ app/src/main/java/com/example/dondesang/BadgeActivity.java
@@ -0,0 +1,39 @@
+package com.example.dondesang;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.dondesang.R;
+import com.example.dondesang.UserActivity;
+import com.example.dondesang.model.User;
+import com.example.dondesang.databinding.FragmentAccountMenuBinding;
+import com.example.dondesang.ui.account.donations.BloodDonationsFragment;
+import com.example.dondesang.ui.account.informations.AccountInformationsFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class BadgeActivity extends AppCompatActivity {
+    private FragmentAccountMenuBinding binding;
+    private FirebaseAuth mAuth;
+    private User user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_badge);
+
+    }
+}
