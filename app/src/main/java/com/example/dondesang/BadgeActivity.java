085e1242b446ee90ed089be8f2d3f49eed55ea21
ThomasClvr <99747179+ThomasClvr@users.noreply.github.com>
2022-12-08 16:23:45 -0500

    Badges modif

--- app/src/main/java/com/example/dondesang/BadgeActivity.java
+++ app/src/main/java/com/example/dondesang/BadgeActivity.java
@@ -1,9 +1,12 @@
 package com.example.dondesang;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
@@ -18,6 +21,7 @@ import com.example.dondesang.model.User;
 import com.example.dondesang.databinding.FragmentAccountMenuBinding;
 import com.example.dondesang.ui.account.donations.BloodDonationsFragment;
 import com.example.dondesang.ui.account.informations.AccountInformationsFragment;
+import com.example.dondesang.ui.account.menu.MenuFragment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -30,10 +34,23 @@ public class BadgeActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private User user;
 
+    Button BackButton2;
+    TextView nobadge;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_badge);
+
+        BackButton2 = findViewById(R.id.BackButton2);
+        nobadge = findViewById(R.id.nobadge);
+
+        BackButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentmenuFragment= new Intent(getApplicationContext(), MenuFragment.class);
+                startActivity(intentmenuFragment);
+            }
+        });
 
     }
 }
